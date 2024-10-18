d8dfaff9f5aafa8a34b95769d7d84090ac234459
Kenneth Edoho <kennethtibele@gmail.com>
2024-10-18 13:50:54 +0100

    Add project files.

---
+++ DatabaseAndFileSystemAssignment3/Models/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DatabaseAndFileSystemAssignment3.Models
+{
+    public class AppDbContext :DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<GallaryItems> Kenneths { get; set; }
+    }
+}
